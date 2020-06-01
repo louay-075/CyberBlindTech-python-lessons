634bb89d43740d7b524a8606206393bc7dd3f44c
louay-075 <65093643+louay-075@users.noreply.github.com>
2020-06-01 14:53:30 -0700

    Add files via upload

---
+++ variable-2.py
@@ -0,0 +1,9 @@
+name = raw_input('Enter Your name :')
+
+last_name = raw_input('Enter your lastname :')
+
+age = int(raw_input('How old are you :'))
+
+grade = float(raw_input('What is your grade :'))
+
+print 'Helloe %s %s , Your age is %d years old and you got %r' % (name,last_name,age,grade)
