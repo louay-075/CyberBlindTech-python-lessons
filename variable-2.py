fca307b01321110d0b8146c2f53ce761a2637b66
louay-075 <65093643+louay-075@users.noreply.github.com>
2020-06-11 14:42:37 -0700

    Update variable-2.py

--- variable-2.py
+++ variable-2.py
@@ -6,4 +6,4 @@ age = int(raw_input('How old are you :'))
 
 grade = float(raw_input('What is your grade :'))
 
-print 'Helloe %s %s , Your age is %d years old and you got %r' % (name,last_name,age,grade)
+print 'Hello %s %s , Your age is %d years old and you got %r' % (name,last_name,age,grade)
