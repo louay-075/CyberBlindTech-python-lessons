3b77b898e176fb1b99ee9d6e0a9bba6af47a0a86
louay-075 <65093643+louay-075@users.noreply.github.com>
2020-06-19 08:26:16 -0700

    Add files via upload

---
+++ lesson4-comments.py
@@ -0,0 +1,15 @@
+# hello 
+# this is a variable called name
+name = 'louay'
+# this is a variable called lastname
+lastname = 'eloudi'
+"""
+this is a multiline comment!
+we will print out these two variables
+name 
+lastname 
+using python
+"""
+
+print name
+print lastname
