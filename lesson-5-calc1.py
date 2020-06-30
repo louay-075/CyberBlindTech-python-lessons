79cd1152e2f71b4bf107ce4735e390d72c99e4c9
louay-075 <65093643+louay-075@users.noreply.github.com>
2020-06-30 14:20:16 -0700

    Add files via upload

---
+++ lesson-5-calc1.py
@@ -0,0 +1,23 @@
+# we will adding numbers
+print 'Adding two numbers!'
+a = int(raw_input('Enter a number :'))
+b = int(raw_input('Enter another number :'))
+print 'The result is :', a + b
+
+# Subtract
+print 'Subtract two numbers!'
+a = int(raw_input('Enter a number :'))
+b = int(raw_input('Enter another number :'))
+print 'The result is :', a - b
+
+# Divide
+print 'Divide two numbers!'
+a = int(raw_input('Enter a number :'))
+b = int(raw_input('Enter another number :'))
+print 'The result is :', a / b
+
+# Multiply
+print 'Multiply two numbers!'
+a = int(raw_input('Enter a number :'))
+b = int(raw_input('Enter another number :'))
+print 'The result is :', a * b
