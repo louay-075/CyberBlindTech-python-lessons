b52725671eb2fb7a776c7fa5e97891fd34b7e468
louay-075 <65093643+louay-075@users.noreply.github.com>
2020-08-18 04:24:06 -0700

    Add files via upload

---
+++ lesson-9-Booleans.py
@@ -0,0 +1,31 @@
+"""
+def check_string(s):
+    if s:
+        return 'it is not empty!!'
+    else:
+        return 'String is empty!!'
+
+print check_string('python')
+"""
+"""
+def check_type(a, b):
+    if type(a) == int and b == int:
+        return True
+    elif type(a) == str and b == str:
+        return True
+    elif type(a) == float and b == float:
+        return True
+    else:
+        return False
+
+print check_type('hello', str)
+"""
+"""
+def check_less(x, y):
+    if x < y:
+        return True
+    else:
+        return False
+
+print check_less(100, 50)
+"""
