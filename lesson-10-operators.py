a1087b90f0c77ce0de1727b73d0493b7e844192c
louay-075 <65093643+louay-075@users.noreply.github.com>
2020-08-22 04:33:10 -0700

    Add files via upload

---
+++ lesson-10-operators.py
@@ -0,0 +1,112 @@
+# Arithmetic operators
+# Examples : Arithmetic operators in Python
+
+x = 15
+y = 4
+
+# Output: x + y = 19
+print 'x + y =', x+y
+
+# Output: x - y = 11
+print 'x - y =',x-y
+
+# Output: x * y = 60
+print 'x * y =',x*y
+
+# Output: x / y = 3.75
+print 'x / y =',x/y
+
+# Output: x ** y = 50625
+print 'x ** y =',x**y
+
+# Output: x % y = 3
+print 'x % y =',x%y
+
+#Comparison operators
+# Examples : Comparison operators in Python
+
+x = 10
+y = 12
+
+# Output: x > y is False
+print 'x > y is', x>y
+
+# Output: x < y is True
+print 'x < y is', x<y
+
+# Output: x == y is False
+print 'x == y is', x==y
+
+# Output: x != y is True
+print 'x != y is', x!=y
+
+# Output: x >= y is False
+print 'x >= y is', x>=y
+
+# Output: x <= y is True
+print 'x <= y is', x<=y
+
+
+
+# Logical operators
+# Examples : Logical Operators in Python
+
+x = True
+y = False
+
+# Output : x and y is False
+print 'x and y is',x and y
+
+# Output: x or y is True
+print 'x or y is',x or y
+
+# Output: not x is False
+print 'not x is',not x
+
+# Assignment operators
+# Examples : Assignment operators in python
+
+a = 5
+a += 5
+# Output: 10
+print a
+
+a -= 3
+# Output: 7
+print a
+
+a *= 2 
+# Output: 14
+print 10
+
+a /= 4
+# Output: 7
+print a
+
+a %= 3
+# Output: 1
+
+# Identity operators
+# Examples: Identity operators in Python
+
+x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+
+# Output: False
+print x1 is not y1
+
+# Output: True
+print x2 is y2
+
+# Membership operators
+# Examples : Membership operators in Python
+
+x = 'Hello world'
+
+# Output: True
+print 'H' in x
+
+# Output: False
+print 'd' not in x
